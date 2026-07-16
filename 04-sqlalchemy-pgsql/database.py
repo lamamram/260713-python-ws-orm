913251f8fd9fd01b1b408ec8db4c5cda1b0ddb97
mlamamra <mlamamra@dawan.fr>
2026-07-16 12:11:37 +0200

    j4: sqlalchemy: connexions pool & session

--- 04-sqlalchemy-pgsql/database.py
+++ 04-sqlalchemy-pgsql/database.py
@@ -16,4 +16,12 @@ pg_db = os.getenv("PG_DB", "default")
 DATABASE_URL = f"postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
 engine = create_engine(
     DATABASE_URL,
+    pool_size=15,  # nombre maximum de connexions persistentes dispos dans le pool pour les utilisateurs
+    max_overflow=5,  # nombre maximum de connexions supplémentaires au-delà du pool_size en cas de pic
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,# les transactions ne sont pas validées automatiquement, il faut appeler session.commit() pour valider
+    autoflush=False, 
+    bind=engine
 )
