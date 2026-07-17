c1553982161ececb65b1a0783828204f6400b535
mlamamra <mlamamra@dawan.fr>
2026-07-17 16:37:06 +0200

    exemple de middleware fastapi: chronométrage du traitement

--- 05-api-orm-catalogue/main.py
+++ 05-api-orm-catalogue/main.py
@@ -2,12 +2,24 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from routers import articles, auteurs
 from exceptions import RessourceNonTrouveException
+import time
 
 app = FastAPI(
     title="API Catalogue",
     description="Premier projet FastAPI — formation Dawan",
     version="0.1.0",
 )
+
+@app.middleware("http")
+async def ajouter_duree_traitement(request: Request, call_next):
+    debut = time.perf_counter()
+    # call_next appelle le prochain middleware (liste chainée) ou le gestionnaire de route FastAPI demandé par l'url client
+    # la response est celle d'un gestionnaire
+    response = await call_next(request)
+    duree = time.perf_counter() - debut
+    # ajouter la durée en tant que en-tête HTTP dans la réponse
+    response.headers["X-Process-Time"] = f"{duree:.4f}s"
+    return response
 
 @app.exception_handler(RessourceNonTrouveException)
 async def resource_not_found_handler(
